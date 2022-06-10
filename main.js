ca1866e532016007156b8e3183c473a867a3265c
David Sosa <david.sosa@singularfactory.com>
2022-06-10 17:43:43 +0100

    API Call

--- main.js
+++ main.js
@@ -23,4 +23,24 @@ const joinValues = () => {
   console.log(submission)
 }
 
-solveButton.addEventListener('click', joinValues)
+const solve = () => {
+
+  const url = 'https://sudoku-solver3.p.rapidapi.com/sudokusolver/';
+  
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '159cb5e053msh672957000ceedfcp172c5cjsn3c838498df74',
+      'X-RapidAPI-Host': 'sudoku-solver3.p.rapidapi.com'
+    },
+    body: '{"input":[0,0,8,9,0,0,4,0,0,0,0,5,6,0,0,0,0,0,3,0,0,7,0,0,6,0,9,5,0,0,0,0,4,0,2,0,0,0,0,0,0,0,0,6,5,0,0,2,0,0,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,8,9,0,0,0,7,0,4,0,0,2,0,0]}'
+  };
+  
+  fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+}
+
+solveButton.addEventListener('click', solve)
